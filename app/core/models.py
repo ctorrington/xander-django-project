97620b67302141158e783005bc6baed2b56d093a
ctorrington1 <ctorrington1@gmail.com>
2023-04-20 13:59:22 +0100

    update

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+
+from django.conf import settings
+
+class UserManager(BaseUserManager):
+    """Custom manager for the User Model."""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save & return a new user."""
+        
+        if not email:
+            raise ValueError('Please enter an email address.')
+        
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create, save & return a new superuser."""
+        
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Model to represent user in the system."""
+    
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    
+    objects = UserManager()
